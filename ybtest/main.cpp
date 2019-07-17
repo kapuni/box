9210dc79351d6ee94f8360f62508e187822975b5
kapuni <910048757@qq.com>
2019-07-17 15:27:23 +0800

    c++

---
+++ ybtest/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = " Kapuni";
+    int characterAge;
+    characterAge = 20;
+    cout << "There is a men named" << characterName << endl;
+    cout << "He is " << characterAge << "years old" << endl;
+
+    characterName =" Mike";
+    cout << "He like singing songs" << characterName << endl;
+    cout << "But not like being" <<  characterAge << endl;
+    return 0;
+}
+
