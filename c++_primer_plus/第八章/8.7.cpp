da83d2ebae5017c0d30c60706bbcc66dbb02e69a
Kapuni <910048757@qq.com>
2019-10-28 23:16:12 +0800

    c++

---
+++ c++_primer_plus/第八章/8.7.cpp
@@ -0,0 +1,12 @@
+//trquote.cpp - different designs
+#include<iostream>
+#include<string>
+using namespace std;
+string version1(const string & s1, const string & s2);
+const string & version2(string & s1, const string &s2);
+const string & version3(string & s1, const string &s2);
+
+int main()
+{
+
+}
