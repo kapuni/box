6c8d540e0d3e939e0790542c64ced2c0769e2301
Kapuni <910048757@qq.com>
2019-12-16 12:37:59 +0800

    c++

---
+++ c++_primer_plus/十八章/18.1rvref/18.1rvref/18.1rvref.cpp
@@ -0,0 +1,19 @@
+//rvref.cpp -- simple uses of rvalue reference
+#include <iostream>
+
+inline double f(double tf) {return 5.0*(tf-32.0)/9.0;}
+
+int main()
+{
+    using namespace std;
+    double tc = 21.5;
+    double && rd1 = 7.07;  //&&为右值 只关联到右值的地址
+    double && rd2 = 1.8 * tc + 32.0;
+    double && rd3 = f(rd2);
+    cout << " tc value and address :" << tc << ", " << &tc << endl;
+    cout << " rd1 value and address :" << rd1 << ", " << &rd1 << endl;
+    cout << " rd2 value and address :" << rd2 << ", " << &rd2 << endl;
+    cout << " rd3 value and address :" << rd3 << ", " << &rd3 << endl;
+    cin.get();
+    return 0;
+}
